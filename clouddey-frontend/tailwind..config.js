f4d2fe979dcb4a9c1af9ae8e32e82af5372d0a17
Ebzefr <151416411+Ebzefr@users.noreply.github.com>
2025-04-10 18:23:54 +0100

    Commit 3

--- clouddey-frontend/tailwind..config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}", // This tells Tailwind to look in all your React files
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
